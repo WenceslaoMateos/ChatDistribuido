36161f07038c8473ebbced830b3e403647280081
Bravlin <bravlinleonel@gmail.com>
2018-11-05 10:44:19 -0300

    Parte http del servidor.

--- Servidor.js
+++ Servidor.js
@@ -1,9 +1,69 @@
 var net = require('net');
+var http = require('http');
 
 //var HOST = '10.9.10.56';
 var PORT = 6969;
 
 var delay = 256;
+
+function Cliente(username, ip, port){
+    this.username = username;
+    this.ip = ip;
+    this.port = port;
+
+    this.getUsername = () => {
+        return this.username;
+    }
+
+    this.getIp = () => {
+        return this.ip;
+    }
+
+    this.getPort = () => {
+        return this.port;
+    }
+}
+
+function RegistroClientes(){
+    this.clientes = [];
+
+    this.registrarCliente = (cliente) => {
+        this.clientes.push(cliente);
+    }
+    
+    this.toJSON = () => {
+        JSON.stringify(this.clientes);
+    }
+}
+
+var registro = new RegistroClientes();
+
+var servidorRegistro = http.createServer((req, res) => {
+    var parseo, query, resultado, direccion, cliente;
+    if (req.method === "GET"){
+        parseo = url.parse(req.url, true);
+        direccion = parseo.pathname;
+        query = parseo.query;
+        if (direccion === '/register'){
+            try {
+                cliente = new Cliente(query.username, query.ip, query.port);
+                registro.registrarCliente(cliente);
+                res.writeHead(200, {
+                    'Date': (new Date()).toString(),
+                    'Content-Length': 2,
+                    'Content-Type': 'text/string'
+                });
+                res.end(registro.toJSON());
+            }
+            catch (e){
+                console.log(e);
+                res.writeHead(404);
+                res.end();
+            }
+        }
+    }
+});
+servidorRegistro.listen(PORT);
 
 var server = net.createServer((sock) => {
     sock.on('data', (data) => {
