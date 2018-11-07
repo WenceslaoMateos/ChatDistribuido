7d05bf540ef5de741cc32b717f663a1e25734676
francolanzi <franco.lanzi96@gmail.com>
2018-11-07 14:09:44 -0300

    Primera version funcional y con mensajes secretos

--- Cliente.js
+++ Cliente.js
@@ -1,60 +1,162 @@
 /* ********DECLARACIÓN DE LIBRERIAS******** */
 var net = require('net');
 var http = require('http');
+var readline = require('readline');
 var readlineSync = require('readline-sync');
-var ip = require("ip");
 
 /* ********DECLARACIÓN DE VARIABLES******** */
-var ipServidor = "127.0.0.1";
-var puertoServidor = 6969;
+var ipServidor;
+var NTP_PORT = 687;
+var HTTP_PORT = 4887;
 
 var offset;
 
-var username = "wence";
+var username;
 
-//ip y puerto donde va a escuchar
-var cliente = new net.Socket();
-var ipCliente = "127.0.0.1";
-//var ipCliente = ip.address();
-var puertoCliente = 6969;
-var conexiones;
+var clienteNTP = new net.Socket();
+var ipCliente;
+var puertoCliente;
+var nodos = new Map();
+var nodo;
 
 
-/* ************I/O DE CONSOLA*************** */
-//var puertoCliente = readlineSync.question('Escriba el puerto del cliente: ');
-//var ipServidor = readlineSync.question('Escriba la ip del servidor: ');
+/* ********I/O DE CONSOLA******** */
+username = readlineSync.question('Escriba su nombre de usuario: ');
+puertoCliente = readlineSync.question('Escriba su numero de puerto: ');
+ipCliente = readlineSync.question('Escriba su IP: ');
+ipServidor = readlineSync.question('Escriba la IP del servidor: ');
 
-/* ******CALCULO DE RELOJES****** */
-/*
-cliente.connect(ipServidor, puertoServidor, () => {
+/* ********CALCULO DE RELOJES******** */
+clienteNTP.connect(NTP_PORT, ipServidor, () =>
+{
     var T1 = (new Date()).getTime();
-    cliente.write(T1.toString());
+    clienteNTP.write(T1.toString());
 });
 
-cliente.on('data', (data) => {
+clienteNTP.on('data', (data) =>
+{
     var T4 = (new Date()).getTime();
     var tiempos = data.toString().split(",");
     var T1 = parseInt(tiempos[0]);
     var T2 = parseInt(tiempos[1]);
     var T3 = parseInt(tiempos[2]);
     offset = ((T2 - T1) + (T3 - T4)) / 2;
-
-    cliente.destroy();
+    console.log('Offset: ' + offset);
+    clienteNTP.destroy();
 });
 
-/* *****REGISTRO POR HTTP*** */
+/* ********CREACIÓN DEL NODO******** */
+var nodo = net.createServer(socket =>
+{
+    socket.on('data', data =>
+    {
+        var datos = JSON.parse(data);
+        if ('username' in datos)
+        {
+            nodos.set(datos.username, new net.Socket());
+            nodos.get(datos.username).connect(datos.port, datos.ip, () =>
+            {
+                console.log('Conectado con ' + datos.username);
+            });
+        }
+        else if ('message' in datos)
+        {
+            if (datos.to == 'all')
+                console.log(datos.from + ': ' + datos.message + ' - ' + new Date(datos.timestamp + datos.offset));
+            else if (datos.to == username)
+                console.log('[' + datos.from + ']: ' + datos.message + ' - ' + new Date(datos.timestamp + datos.offset));
+        }
+    })
+});
+nodo.listen(puertoCliente, () =>
+{
+    console.log('Recibiendo mensajes en ' + ipCliente + ':' + puertoCliente);
+    registroHTTP();
+});
+nodo.on('connection', () =>
+{
+    console.log('Usuario conectado');
+});
 
-http.get("http://" + ipServidor + ":" + puertoServidor + "/register?username=" + encodeURIComponent(username) + "&ip=" + encodeURIComponent(ipCliente) + "&port=" + encodeURIComponent(puertoCliente), (response) => {
-    response.setEncoding('utf8');
-    var datos = "";
-    response.on("data", (data) => {
-        datos += data.toString();
+/* ********REGISTRO POR HTTP******** */
+function registroHTTP()
+{
+    http.get("http://" + ipServidor + ":" + HTTP_PORT + "/register?username=" + encodeURIComponent(username) + "&ip=" + encodeURIComponent(ipCliente) + "&port=" + encodeURIComponent(puertoCliente), (response) =>
+    {
+        response.setEncoding('utf8');
+        var datos = "";
+        response.on("data", (data) =>
+        {
+            datos += data.toString();
+        });
+        response.on("end", () =>
+        {
+            var conexiones = JSON.parse(datos);
+            console.log(conexiones);
+            conectarNodos(conexiones);
+        });
+    }).on("error", (er) =>
+    {
+        console.log(er.toString());
     });
-    response.on("end", () => {
-        conexiones = JSON.parse(datos);
-        console.log(conexiones);
-    });
+}
 
-}).on("error", (er) => {
-    console.log(er.toString());
+/* ********CONEXIÓN CON NODOS******** */
+function conectarNodos(conexiones)
+{
+    conexiones.forEach(c =>
+    {
+        nodos.set(c.username, new net.Socket());
+        nodos.get(c.username).connect(c.port, c.ip, () =>
+        {
+            console.log('Conectado con ' + c.username);
+            nodos.get(c.username).write(JSON.stringify(
+            {
+                username: username,
+                ip: ipCliente,
+                port: puertoCliente
+            }));
+        });
+    });
+}
+
+/* ********ENVÍO DE MENSAJES******** */
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+rl.on('line', (line) =>
+{
+    var datos = line.split('@');
+    var mensaje = {
+        from: username,
+        to: '',
+        message: datos[0],
+        timestamp: (new Date()).getTime(),
+        offset: offset
+    };
+    if (datos.length == 1)
+    {
+        mensaje.to = 'all';
+        mensaje = JSON.stringify(mensaje);
+        for (const [name, nodo] of nodos.entries())
+        {
+            nodo.write(mensaje);
+            console.log('Mensaje enviado a ' + name);
+        }
+        console.log(username + ': ' + line);
+    }
+    else
+    {
+        for (var i = 1; i < datos.length; i++)
+        {
+            mensaje.to = datos[i];
+            var privado = JSON.stringify(mensaje);
+            nodos.get(datos[i]).write(privado);
+            console.log('Mensaje enviado a ' + datos[i]);
+            console.log(username + ': ' + line);
+        }
+    }
+
+    
 });
--- Servidor.js
+++ Servidor.js
@@ -3,36 +3,41 @@ var http = require('http');
 var url = require('url');
 
 /* ********DECLARACIÓN DE VARIABLES******** */
-//var HOST = '10.9.10.56';
-var PORT = 6969;
-
-var delay = 256;
+var NTP_PORT = 687;
+var HTTP_PORT = 4887;
 
 var registroClientes = [];
 
-/* ********OPERACIÓN DE LOS SERVIDORES******** */
-var servidorRegistro = http.createServer((req, res) => {
-    var parseo, query, resultado, direccion, cliente;
-    if (req.method === "GET") {
+/* ********SERVIDOR HTTP******** */
+var servidorRegistro = http.createServer((req, res) =>
+{
+    var parseo, query, direccion, cliente;
+    if (req.method == 'GET')
+    {
         parseo = url.parse(req.url, true);
         direccion = parseo.pathname;
         query = parseo.query;
-        if (direccion === '/register') {
-            try {
-                cliente = {
-                    username: username,
-                    ip: ip,
-                    port: port
+        if (direccion == '/register')
+        {
+            try
+            {
+                cliente =
+                {
+                    username: query.username,
+                    ip: query.ip,
+                    port: query.port
                 };
                 console.log(cliente);
-                registroClientes.push(cliente);
-                res.writeHead(200, {
+                res.writeHead(200,
+                {
                     'Date': (new Date()).toString(),
                     'Content-Type': 'text/string'
                 });
                 res.end(JSON.stringify(registroClientes));
+                registroClientes.push(cliente);
             }
-            catch (e) {
+            catch (e)
+            {
                 console.log(e);
                 res.writeHead(404);
                 res.end();
@@ -40,28 +45,35 @@ var servidorRegistro = http.createServer((req, res) => {
         }
     }
 });
-servidorRegistro.listen(PORT);
+servidorRegistro.listen(HTTP_PORT);
 
-/*
-var server = net.createServer((sock) => {
-    sock.on('data', (data) => {
+/* ********SERVIDOR NTP******** */
+var server = net.createServer((sock) =>
+{
+    sock.on('data', (data) =>
+    {
         var T2 = (new Date()).getTime();
         var T3 = (new Date()).getTime();
         sock.write(data.toString() + "," + T2.toString() + "," + T3.toString());
     });
-    sock.on("end", () => {
+    sock.on("end", () =>
+    {
         console.log('Se ha desconectado el usuario');
     });
-}).listen(PORT, () => {
+}).listen(NTP_PORT, () =>
+{
     console.log('Se ha generado el servidor');
 });
 
-server.on('connection', () => {
+server.on('connection', () =>
+{
     console.log('Se han conectado al servidor');
 })
-server.on('error', () => {
+server.on('error', () =>
+{
     console.log('Ha ocurrido un error');
 })
-server.on('close', () => {
+server.on('close', () =>
+{
     console.log('Se cerro el servidor');
-})*/
+})
